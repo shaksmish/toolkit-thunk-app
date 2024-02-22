5fda2960cf1d6741927f303b60f15f54cfba18b5
Shalki Mishra <s.mishra@spherical-systems.com>
2024-02-22 11:58:08 +0100

    finish app

--- src/App.js
+++ src/App.js
@@ -1,10 +1,34 @@
-import logo from './logo.svg';
 import './App.css';
+import { useEffect} from 'react';
+import { useDispatch, useSelector} from 'react-redux';
+import { getPhotos } from './galleryState';
 
 function App() {
+  const dispatch = useDispatch();
+  const photos = useSelector((state) => state.gallery.photos);
+
+  useEffect(() => {
+    dispatch(getPhotos());
+  }, [dispatch]);
+  console.log(photos);
+
   return (
     <div className="App">
       <h1>PHOTO GALLERY</h1>
+      <p>This is a photo gallery made using redux toolkit and redux thunk</p>
+      <hr />
+      <div className="Gallery">
+        {photos.map(photo => (
+          <img 
+          key={photo.id}
+          alt={photo.author}  
+          src={photo.download_url} 
+          width="400"
+          height="400"
+          />
+        ))}
+      </div>
+      <button>VIEW MORE</button>
     </div>
   );
 }
--- src/galleryState.js
+++ src/galleryState.js
@@ -1,9 +1,9 @@
-import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
+import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
 
-const getPhotos = createAsyncThunk(
+export const getPhotos = createAsyncThunk(
   'photos/getPhotos',
   async() => {
-    const response = await fetch("https://picsum.photos/v2/list?page3&limit=9");
+    const response = await fetch("https://picsum.photos/v2/list?page=3&limit=9");
     const formattedResponse = await response.json();
     return formattedResponse;
  } 
@@ -15,19 +15,22 @@ export const gallerySlice = createSlice({
     photos: [],
     isLoading: false,
   },
+  reducers: {},
   //extraReducers used for Thunk
-  extraReducers:{
-    [getPhotos.pending]: (state) => {
-      state.isLoading = true;
-    },
-    [getPhotos.fulfilled]: (state, action) => {
-      state.photos = action.payload;
-      state.isLoading = false;
-    },
-    [getPhotos.rejected]: (state) => {
-      state.isLoading = false;
-    }
+  extraReducers: (builder) => {
+    builder
+      .addCase(getPhotos.pending, (state) => {
+        state.isLoading = true;
+      })
+      .addCase(getPhotos.fulfilled, (state, action) => {
+        state.photos = action.payload;
+        state.isLoading = false;
+      })
+      .addCase(getPhotos.rejected, (state) => {
+        state.isLoading = false;
+      });
   }
 });
+
 
 export default gallerySlice.reducer;
